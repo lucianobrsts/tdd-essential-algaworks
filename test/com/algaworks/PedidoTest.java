3c0534dcd90704c37e0dac1cc4260d3e84e03067
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-07-30 10:00:23 -0300

    Remoção de códigos comentados

--- test/com/algaworks/PedidoTest.java
+++ test/com/algaworks/PedidoTest.java
@@ -17,9 +17,6 @@ public class PedidoTest {
 	private void assertResumoPedido(double valorTotal, double desconto) {
 		ResumoPedido resumoPedido = pedido.construir().resumo();
 
-//		assertEquals(valorTotal, resumoPedido.getValorTatal(), 0.0001);
-//		assertEquals(desconto, resumoPedido.getDesconto(), 0.0001);
-
 		assertEquals(new ResumoPedido(valorTotal, desconto), resumoPedido);
 	}
 
@@ -30,17 +27,12 @@ public class PedidoTest {
 
 	@Test
 	public void deveCalcularResumoParaUmItemSemDesconto() throws Exception {
-//		pedido.adicionarItem(new ItemPedido("Sabonete", 5.0, 5));
-
 		pedido.comItem(5.0, 5);
 		assertResumoPedido(25.0, 0.0);
 	}
 
 	@Test
 	public void deveCalcularResumoParaDoisitensSemDesconto() throws Exception {
-//		pedido.adicionarItem(new ItemPedido("Sabonete", 3.0, 3));
-//		pedido.adicionarItem(new ItemPedido("Pasta dental", 7.0, 3));
-
 		pedido.comItem(3.0, 3).comItem(7.0, 3);
 
 		assertResumoPedido(30, 0.0);
@@ -48,8 +40,6 @@ public class PedidoTest {
 
 	@Test
 	public void deveAplicarDescontoNa1aFaixa() throws Exception {
-//		pedido.adicionarItem(new ItemPedido("Creme", 20.0, 20));
-
 		pedido.comItem(20.0, 20);
 
 		assertResumoPedido(400, 16.0);
@@ -57,9 +47,6 @@ public class PedidoTest {
 
 	@Test
 	public void deveAplicarDescontoNa2aFaixa() throws Exception {
-//		pedido.adicionarItem(new ItemPedido("Shampo", 15.0, 30));
-//		pedido.adicionarItem(new ItemPedido("�leo", 15.0, 30));
-
 		pedido.comItem(15.0, 30).comItem(15.0, 30);
 
 		assertResumoPedido(900.0, 54.0);
@@ -67,10 +54,6 @@ public class PedidoTest {
 
 	@Test
 	public void deveAplicarDescontoNa3aFaixa() throws Exception {
-//		pedido.adicionarItem(new ItemPedido("Creme", 15.0, 30));
-//		pedido.adicionarItem(new ItemPedido("Óleo", 15.0, 30));
-//		pedido.adicionarItem(new ItemPedido("Shampoo", 10.0, 30));
-
 		pedido.comItem(15.0, 30).comItem(15.0, 30).comItem(10.0, 30);
 
 		assertResumoPedido(1200, 96.0);
