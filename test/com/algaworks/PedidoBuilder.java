b2b7ce9a89066bdb4bedb4b09c8fe35cd826e7f8
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-07-30 09:43:08 -0300

    Refatorado classe para o padrão dataBuilder

---
+++ test/com/algaworks/PedidoBuilder.java
@@ -0,0 +1,29 @@
+package com.algaworks;
+
+import com.algaworks.desconto.CalculadoraDescontoPrimeiraFaiixa;
+import com.algaworks.desconto.CalculadoraDescontoSegundaFaixa;
+import com.algaworks.desconto.CalculadoraDescontoTerceiraFaixa;
+import com.algaworks.desconto.CalculadoraFaixaDesconto;
+import com.algaworks.desconto.SemDesconto;
+
+public class PedidoBuilder {
+
+	private Pedido instancia;
+
+	public PedidoBuilder() {
+		CalculadoraFaixaDesconto calculadoraFaixaDesconto = new CalculadoraDescontoTerceiraFaixa(
+				new CalculadoraDescontoSegundaFaixa(new CalculadoraDescontoPrimeiraFaiixa(new SemDesconto(null))));
+
+		instancia = new Pedido(calculadoraFaixaDesconto);
+	}
+
+	public PedidoBuilder comItem(double valorUnitario, int quantidade) {
+		instancia.adicionarItem(new ItemPedido("Gerado", valorUnitario, quantidade));
+		return this;
+	}
+
+	public Pedido construir() {
+		return instancia;
+	}
+
+}
--- test/com/algaworks/PedidoVendaTest.java
+++ test/com/algaworks/PedidoVendaTest.java
@@ -13,25 +13,17 @@ import com.algaworks.desconto.SemDesconto;
 
 public class PedidoVendaTest {
 
-	private Pedido pedido;
+	private PedidoBuilder pedido;
 
 	@Before
 	public void setup() {
-		CalculadoraFaixaDesconto calculadoraFaixaDesconto = new CalculadoraDescontoTerceiraFaixa(
-				new CalculadoraDescontoSegundaFaixa(new CalculadoraDescontoPrimeiraFaiixa(new SemDesconto(null))));
-
-		pedido = new Pedido(calculadoraFaixaDesconto);
+		pedido = new PedidoBuilder();
 	}
 
 	private void assertResumoPedido(double valorTotal, double desconto) {
-		ResumoPedido resumoPedido = pedido.resumo();
+		ResumoPedido resumoPedido = pedido.construir().resumo();
 		assertEquals(valorTotal, resumoPedido.getValorTatal(), 0.0001);
 		assertEquals(desconto, resumoPedido.getDesconto(), 0.0001);
-	}
-
-	@Test
-	public void devePermitirAdicionarUmItemNoPedido() throws Exception {
-		pedido.adicionarItem(new ItemPedido("Sabonete", 3.0, 10));
 	}
 
 	@Test
@@ -41,43 +33,51 @@ public class PedidoVendaTest {
 
 	@Test
 	public void deveCalcularResumoParaUmItemSemDesconto() throws Exception {
-		pedido.adicionarItem(new ItemPedido("Sabonete", 5.0, 5));
+//		pedido.adicionarItem(new ItemPedido("Sabonete", 5.0, 5));
 
-		ResumoPedido resumoPedido = pedido.resumo();
-
-		assertEquals(25.0, resumoPedido.getValorTatal(), 0.0001);
-		assertEquals(0.0, resumoPedido.getDesconto(), 0.0001);
-
+		pedido.comItem(5.0, 5);
+		assertResumoPedido(25.0, 0.0);
 	}
 
 	@Test
 	public void deveCalcularResumoParaDoisitensSemDesconto() throws Exception {
-		pedido.adicionarItem(new ItemPedido("Sabonete", 3.0, 3));
-		pedido.adicionarItem(new ItemPedido("Pasta dental", 7.0, 3));
+//		pedido.adicionarItem(new ItemPedido("Sabonete", 3.0, 3));
+//		pedido.adicionarItem(new ItemPedido("Pasta dental", 7.0, 3));
+
+		pedido.comItem(3.0, 3).comItem(7.0, 3);
 
 		assertResumoPedido(30, 0.0);
 	}
 
 	@Test
 	public void deveAplicarDescontoNa1aFaixa() throws Exception {
-		pedido.adicionarItem(new ItemPedido("Creme", 20.0, 20));
+//		pedido.adicionarItem(new ItemPedido("Creme", 20.0, 20));
+
+		pedido.comItem(20.0, 20);
 
 		assertResumoPedido(400, 16.0);
 	}
 
 	@Test
 	public void deveAplicarDescontoNa2aFaixa() throws Exception {
-		pedido.adicionarItem(new ItemPedido("Shampo", 15.0, 30));
-		pedido.adicionarItem(new ItemPedido("�leo", 15.0, 30));
+//		pedido.adicionarItem(new ItemPedido("Shampo", 15.0, 30));
+//		pedido.adicionarItem(new ItemPedido("�leo", 15.0, 30));
+
+		pedido.comItem(15.0, 30)
+		.comItem(15.0, 30);
 
 		assertResumoPedido(900.0, 54.0);
 	}
 
 	@Test
 	public void deveAplicarDescontoNa3aFaixa() throws Exception {
-		pedido.adicionarItem(new ItemPedido("Creme", 15.0, 30));
-		pedido.adicionarItem(new ItemPedido("Óleo", 15.0, 30));
-		pedido.adicionarItem(new ItemPedido("Shampoo", 10.0, 30));
+//		pedido.adicionarItem(new ItemPedido("Creme", 15.0, 30));
+//		pedido.adicionarItem(new ItemPedido("Óleo", 15.0, 30));
+//		pedido.adicionarItem(new ItemPedido("Shampoo", 10.0, 30));
+
+		pedido.comItem(15.0, 30)
+		.comItem(15.0, 30)
+		.comItem(10.0, 30);
 
 		assertResumoPedido(1200, 96.0);
 	}
